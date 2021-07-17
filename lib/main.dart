baa720fa54dd908ab8807c8626de7be6d8b39587
Oreoluwa <solaojoore@yahoo.com>
2021-07-18 00:32:40 +0100

    Xylophonee

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void SoundMan (int soundgod) {
+    final player = AudioCache();
+    player.play('note$soundgod.wav');
+  }
+  Expanded buildkey({Color color, int soundgod}){
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: (){
+          SoundMan(soundgod);
+        },
+        ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildkey(color: Colors.red, soundgod: 1),
+              buildkey(color: Colors.orange, soundgod: 2),
+              buildkey(color: Colors.yellow, soundgod: 3),
+              buildkey(color: Colors.green, soundgod: 4),
+              buildkey(color: Colors.teal, soundgod: 5),
+              buildkey(color: Colors.blue, soundgod: 6),
+              buildkey(color: Colors.purple, soundgod: 7),
+
+
+
+
+            ],
+          ),
         ),
       ),
     );
